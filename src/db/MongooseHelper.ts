86fdba06c4e79da55c6e3ec5157202229618bc30
Voroniyx <julian.ruge@icloud.com>
2024-02-17 01:23:56 +0100

    changed the way how to connect

--- src/db/MongooseHelper.ts
+++ src/db/MongooseHelper.ts
@@ -7,12 +7,6 @@ import { set, connect } from 'mongoose';
 export default class MongooseHelper {
     public static async connectToDatabase(uri: string) {
         set("strictQuery", true);
-        await connect(uri)
-            .catch((err) => {
-                throw err;
-            })
-            .then(() =>
-                console.log("Connected to Database"),
-            );
+        return await connect(uri);
     }
 }
