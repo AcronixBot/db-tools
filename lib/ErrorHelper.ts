1c8fd00266a64e36b99de38cd929b9277e6a8658
Voroniyx <91881990+Voroniyx@users.noreply.github.com>
2024-03-22 13:56:59 +0100

    Create ErrorHelper.ts

---
+++ lib/ErrorHelper.ts
@@ -0,0 +1,6 @@
+//TODO https://github.com/discordjs/discord.js/blob/main/packages/discord.js/src/errors/DJSError.js
+export class BackupError extends Error {
+    constructor() {
+        super()
+    }
+}
