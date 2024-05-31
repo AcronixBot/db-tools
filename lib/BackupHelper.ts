742050b0f5b8819456812d569f9f3969fcbda4e3
Voroniyx <91881990+Voroniyx@users.noreply.github.com>
2024-06-01 00:51:53 +0200

    Added a lip implementation (#4)
    * Create BackupHelper.ts
    
    * Create ErrorHelper.ts
    
    * BackupHelper class implementation and other things
    
    * bug fix
    
    * additions for package release
    
    * build fix
    
    * added a function to resolve path options properly for linux systems
    
    * raised version
    
    * fixxed some bugs and added an integration test
    
    * cleanup

---
+++ lib/BackupHelper.ts
@@ -0,0 +1,133 @@
+import File from "@lib/util/FileHelper.js";
+import { Collection, connect, disconnect, set } from "mongoose";
+import { DbHelperError } from "@lib/util/ErrorHelper.js";
+
+interface BackupOptions {
+  outDir: `\\${string}\\`;
+  connectionString: string;
+}
+
+export interface BackUpResult {
+  timestamp: number;
+  fileName: string;
+  outDirPath: string;
+}
+
+export class BackupHelper {
+  private options: BackupOptions;
+
+  constructor(options: BackupOptions) {
+    this.options = options;
+  }
+  /**
+   *
+   * @param {String} path
+   * @returns
+   */
+  private normalizePathForLinux(path: string) {
+    if (process.platform === "linux") {
+      return path.replaceAll("\\", "/");
+    } else {
+      return path;
+    }
+  }
+
+  public async create(): Promise<BackUpResult> {
+    const startTimestamp = new Date().getTime();
+    const database = this.extractDatabaseFromURI(this.options.connectionString);
+    if (!database) {
+      throw new Error(
+        "Could not find database with the provieded connection string while trying to create a database backup!"
+      );
+    }
+
+    const mongooseConnection = await this.connectToDatabase(
+      this.options.connectionString
+    );
+
+    const connection = mongooseConnection.connections.find(
+      (v) => v.name === database
+    );
+
+    if (connection === undefined) {
+      throw new Error(
+        "Could not find connection while trying to create a database backup"
+      );
+    }
+
+    const collections = await connection.db.collections();
+
+    if (collections.length > 0) {
+      //FIX Everything Syncron from this point
+
+      //Declare dir paths
+      const tempDir = process.cwd() + this.normalizePathForLinux(`\\temp\\`);
+      const outputDir =
+        process.cwd() + this.normalizePathForLinux(this.options.outDir);
+
+      //Create Temp Dir
+      try {
+        File.createDirectoryIfNotExists(tempDir);
+      } catch (e) {
+        throw new DbHelperError(e);
+      }
+
+      // iterate over all collections and write the data to the json files
+
+      const endResult = await Promise.allSettled(
+        //@ts-expect-error
+        collections.map((collection) => this.handleCollection(collection))
+      ).then(async (results) => {
+        // Check if all promises were fulfilled
+        const allFulfilled = results.every(
+          (result) => result.status === "fulfilled"
+        );
+        if (allFulfilled) {
+          const endResult = await File.createZipFromDirectory(
+            tempDir,
+            outputDir
+          );
+          return endResult;
+        } else {
+          throw new DbHelperError(
+            "Some collections were not handled successfully."
+          );
+        }
+      });
+      disconnect();
+      return {
+        fileName: endResult.fileName,
+        outDirPath: endResult.outPath,
+        timestamp: new Date().getTime() - startTimestamp,
+      };
+    }
+  }
+
+  private extractDatabaseFromURI(uri: string) {
+    //mongodb+srv://myDatabaseUser:D1fficultP%40ssw0rd@cluster0.example.mongodb.net/database?retryWrites=true&w=majority
+    //* split(':')[2] -> D1fficultP%40ssw0rd@cluster0.example.mongodb.net/database?retryWrites=true&w=majority
+    //* split('/')[1] -> database?retryWrites=true&w=majority
+    //* split('?')[0] -> database
+    return uri.split(":")[2].split("/")[1].split("?")[0];
+  }
+
+  private async connectToDatabase(uri: string) {
+    set("strictQuery", true);
+    return await connect(uri);
+  }
+
+  private async handleCollection(collection: Collection) {
+    const foundedEntry = collection.find();
+    const fileName =
+      process.cwd() +
+      this.normalizePathForLinux(`\\temp\\${collection.collectionName}.json`);
+    if (!foundedEntry) {
+      File.createFileIfNotExists(fileName, []);
+    } else {
+      File.createFileIfNotExists(
+        fileName,
+        JSON.stringify(await foundedEntry.toArray(), null, 2)
+      );
+    }
+  }
+}
---
+++ lib/util/FileHelper.ts
@@ -0,0 +1,160 @@
+import fs from "fs";
+import { zip } from "zip-a-folder";
+import * as path from "path";
+import { DbHelperError } from "@lib/util/ErrorHelper.js";
+
+export default class File {
+  public static async createDirectoryIfNotExists(directoryPath: string) {
+    try {
+      const directoryExists = await fs.promises
+        .stat(directoryPath)
+        .then((stat) => stat.isDirectory())
+        .catch(() => false);
+
+      if (!directoryExists) {
+        await fs.promises.mkdir(directoryPath, { recursive: true });
+        return true;
+      }
+    } catch (error) {
+      throw new DbHelperError(`An error occured: ${error.message}`, error);
+    }
+
+    return false;
+  }
+
+  public static async createFileIfNotExists(fileName: string, content: any) {
+    try {
+      const fileExists = await fs.promises
+        .stat(fileName)
+        .then((stat) => stat.isFile())
+        .catch(() => false);
+
+      if (!fileExists) {
+        fs.writeFile(fileName, content, (e) => {
+          if (e) {
+            throw new DbHelperError(
+              `The file ${fileName} could not be created: ${e.message}`,
+              e
+            );
+          } else {
+            return true;
+          }
+        });
+      }
+    } catch (error) {
+      throw new DbHelperError(`An error occured: ${error.message}`, error);
+    }
+
+    return false;
+  }
+
+  public static async deleteDirectoryIfExists(directoryPath: string) {
+    try {
+      const directoryExists = await fs.promises
+        .stat(directoryPath)
+        .then((stat) => stat.isDirectory())
+        .catch(() => false);
+      if (directoryExists) {
+        await fs.promises.rm(directoryPath, { recursive: true });
+        return true;
+      }
+    } catch (error) {
+      throw new DbHelperError(`An error occured: ${error.message}`);
+    }
+
+    return false;
+  }
+
+  public static async createZipFromDirectory(
+    dir: string,
+    outDir: string
+  ): Promise<{
+    created: boolean;
+    outPath: string | null;
+    fileName: string | null;
+  }> {
+    if (!fs.existsSync(dir)) {
+      return {
+        created: false,
+        outPath: null,
+        fileName: null,
+      };
+    }
+
+    const createZipFromDirectoryResult = await Promise.allSettled([
+      File.createDirectoryIfNotExists(outDir),
+    ])
+      .then(async (result) => {
+        const allFulfilled = result.every(
+          (result) => result.status === "fulfilled"
+        );
+        if (allFulfilled) {
+          const zipCreatendResult = await File._createZip(dir, outDir);
+          if (zipCreatendResult.result) {
+            const deletedTempDirs = await Promise.allSettled([
+              File.deleteDirectoryIfExists(dir),
+            ])
+              .then((result) => {
+                //@ts-ignore
+                if (result.some((v) => v.value === false)) {
+                  return false;
+                }
+                const dirDeleted = result.every(
+                  (r) => r.status === "fulfilled"
+                );
+                return dirDeleted;
+              })
+              .catch((e) => {
+                throw new DbHelperError(
+                  `An error occurred while deleting the temp directory: ${e.message}`,
+                  e
+                );
+              });
+
+            return {
+              created: deletedTempDirs,
+              outPath: zipCreatendResult.outpath,
+              fileName: zipCreatendResult.fileName,
+            };
+          }
+        } else {
+          throw new DbHelperError(
+            "Some collections were not handled successfully.",
+            result.filter((r) => r.status === "rejected")
+          );
+        }
+      })
+      .catch((error) => {
+        throw new DbHelperError(
+          `An error occurred while checking/creating the directory: ${error.message}`
+        );
+      });
+
+    return createZipFromDirectoryResult;
+  }
+
+  private static async _createZip(dir: string, outDir: string) {
+    const fileName = `backup_${new Date().getTime()}.zip`;
+    const zipPath = path.normalize(path.resolve(outDir, fileName));
+    var zipCreatedResult = false;
+    zipCreatedResult = await Promise.allSettled([zip(dir, zipPath)])
+      .then((result) => {
+        const allFulfilled = result.every(
+          (result) => result.status === "fulfilled"
+        );
+        if (allFulfilled) {
+          return true;
+        } else {
+          throw new DbHelperError(
+            "Some collections were not handled successfully.",
+            result.filter((r) => r.status === "rejected")
+          );
+        }
+      })
+      .catch((e) => {
+        throw new DbHelperError(e);
+      });
+
+    return { outpath: zipPath, result: zipCreatedResult, fileName: fileName };
+  }
+}
